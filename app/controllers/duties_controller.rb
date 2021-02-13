f742371969d86bfb3ce784b2a00cb2364fadc881
RYOTA-EAST <70444118+RYOTA-EAST@users.noreply.github.com>
2021-02-14 01:28:06 +0900

    ２人のユーザーで当番表を作成

--- app/controllers/duties_controller.rb
+++ app/controllers/duties_controller.rb
@@ -1,8 +1,22 @@
 class DutiesController < ApplicationController
   def index
     @user = User.all
+    @duty = Duty.all
   end
   def new
     @duty = Duty.new
+  end
+  def create
+    @duty = Duty.new(duty_params)
+    if @duty.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def duty_params
+    params.require(:duty).permit(:name, user_ids: [])
   end
 end
