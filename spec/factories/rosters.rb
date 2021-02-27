aa1a12cae2c2b31a2a214e9b9a4571f5a482edf2
RYOTA-EAST <70444118+RYOTA-EAST@users.noreply.github.com>
2021-02-28 02:20:55 +0900

    factorybotの日付修正

--- spec/factories/rosters.rb
+++ spec/factories/rosters.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :roster do
-    date {Faker::Date.forward(days: 5)}
+    date {Faker::Date.between(from: 13.days.ago, to: 3.days.from_now)}
     association :duty
     association :user
   end
