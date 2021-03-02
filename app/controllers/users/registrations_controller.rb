4ef3da1526613124f931cd52ddc6cfe5ee480ed0
RYOTA-EAST <70444118+RYOTA-EAST@users.noreply.github.com>
2021-03-02 20:55:34 +0900

    nicknameも編集可能

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,11 +2,12 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
-  before_action :duty_all, only: [:new, :create, :edit]
+  before_action :configure_account_update_params, only: [:update]
+  before_action :duty_all, only: [:edit, :update]
 
   # GET /resource/sign_up
   def new
+    @dutyall = Duty.all
     super
   end
 
@@ -21,9 +22,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+    super
+  end
 
   # DELETE /resource
   # def destroy
@@ -42,7 +43,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # protected
   private
   def duty_all
-    @dutyall = Duty.all
+    @dutyall = Duty.find(DutyUser.where(user_id:current_user.id).pluck(:duty_id))
   end
 
   # If you have extra params to permit, append them to the sanitizer.
@@ -51,10 +52,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
+  end
+  
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
   #   super(resource)
